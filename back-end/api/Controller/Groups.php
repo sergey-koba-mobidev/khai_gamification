3dbe5ae5699ed6b6a65232afffadf9fbc4755645
Alhantro <alhantro@gmail.com>
2014-11-04 22:02:56 +0400

    routes and controllers

--- back-end/api/Controller/Groups.php
+++ back-end/api/Controller/Groups.php
@@ -4,7 +4,7 @@ namespace App\Controller;
  
 class Fairies extends \App\Page {
  
-    public function get_group_info() {
+    public function action_get_group_info() {
  		
     	$id = $this->request->param('id');
         return $gamif->db->query('select')->table('groups')->where('id',$id)->execute();
--- back-end/api/Controller/Students.php
+++ back-end/api/Controller/Students.php
@@ -4,8 +4,8 @@ namespace App\Controller;
  
 class Fairies extends \App\Page {
  
-    public function get_students() {
-    	
+    public function action_get_students() {
+
     	$id = $this->request->param('id');
  
         return $gamif->db->query('select')->table('students')->where('group_id',$id)->execute();
