77aad149333da23ad7aa87084bcbc0793bc58e37
Alhantro <alhantro@gmail.com>
2014-11-22 07:19:25 +0400

    добавил поиск, немного изменил контроллеры

--- back-end/api/Pixie/classes/App/Controller/Students.php
+++ back-end/api/Pixie/classes/App/Controller/Students.php
@@ -15,11 +15,12 @@ class Students extends \App\Page {
     public function action_getList() {
     	if($this->request->param('id'))
     	{
-    		$result = $pixie->orm->get('student')->where('group_id',$id)->find_all();
+            $id = $this->request->param('id');
+    		$result = $pixie->orm->get('student')->where('group_id',$id)->find_all()->as_array(true);
     	}
     	else
     	{
-    		$result = $pixie->orm->get('student')->find_all();
+    		$result = $pixie->orm->get('student')->find_all()->as_array(true);
     	}
     	return json_encode($result);
  		die();
